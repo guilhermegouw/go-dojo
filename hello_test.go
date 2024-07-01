b0dd5c88e6c62a74e4ddb78972ea0e25a3f9d6f2
guilhermegouw <guilherme.gouw@gmail.com>
2024-07-01 19:10:28 -0300

    Added multi-language feature

--- hello_test.go
+++ hello_test.go
@@ -1,16 +1,29 @@
 package main
 
-import "testing"
+import (
+	"fmt"
+	"testing"
+)
 
 func TestHello(t *testing.T) {
 	t.Run("saying hello to people", func(t *testing.T) {
-		got := Hello("Guilherme")
+		got := Hello("Guilherme", "English")
 		want := "Hello, Guilherme!"
-		t.Errorf("got %q want %q", got, want)
+		assertCorrectMessage(t, got, want)
 	})
 	t.Run("say 'Hello, World when an empty string is supplied", func(t *testing.T) {
-		got := Hello("")
+		got := Hello("", "")
 		want := "Hello, World!"
+		assertCorrectMessage(t, got, want)
+	})
+	t.Run("in Spanish", func(t *testing.T) {
+		got := Hello("Luciana", "Spanish")
+		want := "Hola, Luciana!"
+		assertCorrectMessage(t, got, want)
+	})
+	t.Run("in French", func(t *testing.T) {
+		got := Hello("Luciana", "French")
+		want := "Bonjour, Luciana!"
 		assertCorrectMessage(t, got, want)
 	})
 }
@@ -18,6 +31,8 @@ func TestHello(t *testing.T) {
 func assertCorrectMessage(t testing.TB, got, want string) {
 	t.Helper()
 	if got != want {
+		fmt.Printf("Got: %q, Lenght: %d\n", got, len(got))
+		fmt.Printf("Want: %q, Lenght: %d\n", want, len(want))
 		t.Errorf("got %q want %q", got, want)
 	}
 }
