a5f02f5779e25690784af2ce0ab13798062d52a2
guilhermegouw <guilherme.gouw@gmail.com>
2024-07-04 07:40:41 -0300

    added SumAllTails func.

--- array_and_slices/sum.go
+++ array_and_slices/sum.go
@@ -16,3 +16,16 @@ func SumAll(arrays ...[]int) []int {
 	}
 	return sums
 }
+
+func SumAllTails(arrays ...[]int) []int {
+	var sums []int
+
+	for _, numbers := range arrays {
+		if len(numbers) == 0 {
+			sums = append(sums, 0)
+		} else {
+			sums = append(sums, Sum(numbers[1:]))
+		}
+	}
+	return sums
+}
